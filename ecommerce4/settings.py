11b73c348b652a7405a6e8901a206567290bc26f
Almazon <d.almaz0140@gmail.com>
2022-12-08 10:49:43 +0600

    Debug is falsing

--- ecommerce4/settings.py
+++ ecommerce4/settings.py
@@ -13,7 +13,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-m@2)_ts4nzbzk6zn%c-o0qu(+4*n^p9o27yzx*8d4a-_gy!9@%'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     'localhost:8000',
